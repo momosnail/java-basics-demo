74e6ac212a609d9e785018fcb8a17cf4f5f3a37c
momosnail <w550218658@gmail.com>
2017-07-12 00:04:37 +0800

    8
    复制文件标准写法

--- 标准复制文件操作/CopyJpgFileDemo.java
+++ 标准复制文件操作/CopyJpgFileDemo.java
@@ -3,6 +3,8 @@ package day20;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 /*
  * 标准复制文件操作
@@ -12,6 +14,9 @@ import java.io.IOException;
 public class CopyJpgFileDemo {
 
 	public static void main(String[] args) throws IOException {
+		
+		//字节流 复制文件 标准写法
+		/*
 		FileInputStream fis=new FileInputStream("/Users/wgl/Documents/1.pic_hd.jpg");
 		FileOutputStream fos=new FileOutputStream("/Users/wgl/Documents/1131/可爱.jpg");
 		//理论上比 一个字节一个字节读取的read() 快1025倍
@@ -23,5 +28,28 @@ public class CopyJpgFileDemo {
 		
 		fis.close();
 		fos.close();
+		*/
+		
+		//字符流1
+		/*
+		FileReader fr=new FileReader("wgl.txt");
+		int by=0;
+		while ((by=fr.read())!=-1) {
+			System.out.println((char)by);
+		}
+		fr.close();
+		*/
+		
+		//字符流2 复制文件 标准写法
+		FileReader fr=new FileReader("wgl.txt");
+		FileWriter fw=new FileWriter("fos4.txt");
+		char[] bys=new char[1024];
+		int len=0;
+		while ((len=fr.read(bys))!=-1) {
+			System.out.println(new String(bys, 0, len));
+			fw.write(bys, 0, len);
+		}
+		fr.close();
+		fw.close();
 	}
 }
