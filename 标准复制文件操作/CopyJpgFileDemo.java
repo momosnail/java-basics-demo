08fede7b1b15ef809aa984b292f50b0052af6495
momosnail <w550218658@gmail.com>
2017-07-11 23:39:47 +0800

    8
    标准复制文件操作

---
+++ 标准复制文件操作/CopyJpgFileDemo.java
@@ -0,0 +1,26 @@
+package day20;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+/*
+ * 标准复制文件操作
+ * 		int read(byt[] b):一次读取一个字节数组，
+ * 						返回值其实是实际读取的字节个数
+ */
+public class CopyJpgFileDemo {
+
+	public static void main(String[] args) throws IOException {
+		FileInputStream fis=new FileInputStream("/Users/wgl/Documents/1.pic_hd.jpg");
+		FileOutputStream fos=new FileOutputStream("/Users/wgl/Documents/1131/可爱.jpg");
+		byte[] bys=new byte[1024];
+		int b=0;
+		while ((b=fis.read(bys))!=-1) {
+			fos.write(bys, 0, b);
+		}
+		
+		fis.close();
+		fos.close();
+	}
+}
