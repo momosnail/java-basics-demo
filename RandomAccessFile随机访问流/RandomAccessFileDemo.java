d5c49718883b17c0c1901e1889314b07e06aa52f
momosnail <w550218658@gmail.com>
2017-07-17 00:41:04 +0800

    14

--- RandomAccessFile随机访问流/RandomAccessFileDemo.java
+++ RandomAccessFile随机访问流/RandomAccessFileDemo.java
@@ -24,13 +24,15 @@ public class RandomAccessFileDemo {
 
 		int i = raf.readInt();
 		System.out.println(i);
-		// 该文件指针可以通过 getFilePointer方法读取，并通过 seek 方法设置。
+		// 该文件指针可以通过 getFilePointer方法读取，并通过 seek 方法设置。一个int类型4个字／		//节，所以是：4
 		System.out.println("当前文件的指针位置是：" + raf.getFilePointer());
 
 		char ch = raf.readChar();
 		System.out.println(ch);
+		//一个char类型占2个字节 所以：4+2=6
 		System.out.println("当前文件的指针位置是：" + raf.getFilePointer());
-
+		//readUTF()方法说明：从此文件读取一个字符串。该字符串已使用 UTF-8 修改版格式进行编码。
+		//从当前文件指针开始读取前两个字节，类似于使用 readUnsignedShort。此值给出已编码字符串中		//随后的字节数，而不是结果字符串的长度。随后的字节然后解释为 UTF-8 修改版格式的字节编码字			//符，并转换为字符。 中国：每个字符占三个字节是6， 所以6+2+6=14
 		String s = raf.readUTF();
 		System.out.println(s);
 		System.out.println("当前文件的指针位置是：" + raf.getFilePointer());
